e0736953efbb7d0a82f839219e4608ba3fefd8e6
Milosz Slezak <miloszoxo1982@gmail.com>
2023-07-21 11:42:57 +0100

    adds error handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Article from "./pages/Article";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { createContext, useState } from "react";
+import ErrorHandling from "./components/ErrorHandling";
 export const UserContext = createContext(null);
 function App() {
   const [user, setUser] = useState("");
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/article/:article_id" element={<Article />} />
           <Route path="/topics/:topic" element={<Home />}></Route>
+          <Route path="*" element={<ErrorHandling />} />
         </Routes>
       </>
     </UserContext.Provider>
